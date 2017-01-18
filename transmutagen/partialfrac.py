a3b2e2606ff67070ebf5da49b7f199993fe4fb30
Aaron Meurer <asmeurer@gmail.com>
2017-01-18 15:10:47 -0500

    Start writing MatrixNumPyPrinter

--- transmutagen/partialfrac.py
+++ transmutagen/partialfrac.py
@@ -1,5 +1,8 @@
 from sympy import (symbols, fraction, nsimplify, intervals, div, LC, Add,
-    degree, re, together, expand_complex)
+    degree, re, together, expand_complex, Mul)
+
+from sympy.printing.lambdarepr import NumPyPrinter
+from sympy.printing.precedence import precedence
 
 from sympy.utilities.decorator import conserve_mpmath_dps
 
@@ -65,3 +68,42 @@ def thetas_alphas_to_expr(thetas, alphas, alpha0):
 
     return alpha0 + Add(*[re_form.subs({alpha: al, theta: th}) for th,
         al in zip(thetas, alphas)])
+
+class MatrixNumPyPrinter(NumPyPrinter):
+    """
+    Print an expression for numpy assuming the variables are matrices
+
+    Prints inversions as solve() and multiplication of nonconstants as @.
+
+    """
+    def _print_Mul(self, expr):
+        prec = precedence(expr)
+
+        pows = [i for i in expr.args if i.is_Pow and i.exp < 0]
+        if len(pows) > 1:
+            raise NotImplementedError("Need exactly one inverted Pow, not %s" % len(pows))
+
+        if not pows:
+            consts = [self._print(self.parenthesize(i, prec)) for i in expr.args if i.is_Number]
+            rest = [self._print(self.parenthesize(i, prec)) for i in expr.args if not i.is_Number]
+            if consts and rest:
+                return '*'.join(consts) + '*' + '@'.join(rest)
+            else:
+                return '*'.join(consts) + '@'.join(rest)
+
+        [pow] = pows
+
+        rest = Mul(*[i for i in expr.args if i != pow])
+
+        return 'solve(%s, %s)' % (self._print(1/pow), self._print(rest))
+
+    def _print_Add(self, expr):
+        coeff, rest = expr.as_coeff_Add()
+        if coeff:
+            # This is a custom object that automatically creates an
+            # identity*coeff array of the right shape.
+            eye = 'autoeye(%s) + ' % self._print(coeff)
+        else:
+            return super()._print_Add(rest)
+
+        return eye + self._print(rest)
