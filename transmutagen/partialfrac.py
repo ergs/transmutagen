0a3cd80b739b7f2b538c499f0c721af076826c41
Aaron Meurer <asmeurer@gmail.com>
2017-01-17 20:16:42 -0500

    Some fixes to partialfrac
    - Odd degrees are not yet supported.
    - Set mpmath precision (until upstream SymPy fixed).
    - Fix comment.
    - Evalf alpha0.
    - Add function to convert back.

--- transmutagen/partialfrac.py
+++ transmutagen/partialfrac.py
@@ -1,7 +1,10 @@
-from sympy import symbols, fraction, nsimplify, intervals, div, LC
+from sympy import symbols, fraction, nsimplify, intervals, div, LC, Add, degree
+
+from sympy.utilities.decorator import conserve_mpmath_dps
 
 t = symbols('t')
 
+@conserve_mpmath_dps
 def theta_alphas(rat_func, prec, eps=None):
     """
     Do a partial fraction decomposition of rat_func
@@ -23,17 +26,26 @@ def theta_alphas(rat_func, prec, eps=None):
     to 10**-prec but it may need to be set smaller if there are roots smaller
     than ~1/10 to get full precision.
     """
+    import mpmath
+    mpmath.mp.dps = prec
+
     rational_rat_func = nsimplify(rat_func)
     num, den = fraction(rational_rat_func)
+
+    if degree(den) % 1:
+        raise NotImplementedError("Odd degrees are not yet supported")
+
     # Note, eps is NOT the precision. It's the length of the interval.
     # If a root is small (say, on the order of 10**-N), then eps will need to be 10**(-N - d)
-    # to get d digits of precision. For our exp(-t) approximations, the roots (thetas) are all
+    # to get d digits of precision. For our exp(-t) approximations, the roots
+    # (thetas) are all
     # within order 10**-1...10**1, so eps is *roughly* the precision.
     eps = eps or 10**-prec
 
     roots = intervals(den, all=True, eps=eps)[1]
-    # eps ought to be small enough that either side of the interval is the precision we want,
-    # but for the sake of with smaller eps, take the average (center of the rectangle)
+    # eps ought to be small enough that either side of the interval is the
+    # precision we want, but take the average (center of the rectangle)
+    # anyway.
     # XXX: Make sure to change the evalf precision if eps is lowered.
     thetas = [((i + j)/2).evalf(prec) for ((i, j), _) in roots]
     error = [(j - i).evalf(prec) for ((i, j), _) in roots]
@@ -42,5 +54,9 @@ def theta_alphas(rat_func, prec, eps=None):
         q, r = div(den, t - theta)
         alpha = (num/q).evalf(prec, subs={t: theta})
         alphas.append(alpha)
-    alpha0 = LC(num)/LC(den)
+    alpha0 = (LC(num)/LC(den)).evalf(prec)
     return thetas, alphas, alpha0
+
+def thetas_alphas_to_expr(thetas, alphas, alpha0):
+    return alpha0 + Add(*[alpha/(t - theta) for theta,
+        alpha in zip(thetas, alphas)])
