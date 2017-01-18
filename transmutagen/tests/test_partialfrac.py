3f873287869f555e7380b1c51957faac5e0958a5
Aaron Meurer <asmeurer@gmail.com>
2017-01-18 16:49:37 -0500

    Use assert_array_equal in the tests

--- transmutagen/tests/test_partialfrac.py
+++ transmutagen/tests/test_partialfrac.py
@@ -1,6 +1,7 @@
 from sympy import together, expand_complex, re, im, symbols
 
 import numpy as np
+from numpy.testing import assert_array_equal
 
 from pytest import raises
 
@@ -22,23 +23,23 @@ def test_autoeye():
     raises(ValueError, lambda: e + np.array([[1, 2]]))
 
     res = e + np.array([[1, 2], [3, 4]])
-    assert np.all(res == np.array([[3, 2], [3, 6]]))
+    assert_array_equal(res, np.array([[3, 2], [3, 6]]))
     assert res.dtype == int
 
     res = np.array([[1, 2], [3, 4]]) + e
-    assert np.all(res == np.array([[3, 2], [3, 6]]))
+    assert_array_equal(res, np.array([[3, 2], [3, 6]]))
     assert res.dtype == int
 
     res = e + np.array([[1., 2.], [3., 4.]])
-    assert np.all(res == np.array([[3., 2.], [3., 6.]]))
+    assert_array_equal(res, np.array([[3., 2.], [3., 6.]]))
     assert res.dtype == float
 
     res = e + np.matrix([[1, 2], [3, 4]])
-    assert np.all(res == np.matrix([[3, 2], [3, 6]]))
+    assert_array_equal(res, np.matrix([[3, 2], [3, 6]]))
     assert res.dtype == int
     assert isinstance(res, np.matrix)
 
     res = np.matrix([[1, 2], [3, 4]]) + e
-    assert np.all(res == np.matrix([[3, 2], [3, 6]]))
+    assert_array_equal(res, np.matrix([[3, 2], [3, 6]]))
     assert res.dtype == int
     assert isinstance(res, np.matrix)
