1fc7f001d66eb63b30cf9665aa6b65af7491c2a9
Aaron Meurer <asmeurer@gmail.com>
2017-01-26 13:28:25 -0500

    Add partialfrac and util to __init__.py

--- transmutagen/__init__.py
+++ transmutagen/__init__.py
@@ -3,3 +3,13 @@ from .transmutagen import (general_rat_func, nsolve_intervals, nsolve_points,
 
 __all__ = ['general_rat_func', 'nsolve_intervals', 'nsolve_points',
     'plot_in_terminal', 'log_function_args', 'CRAM_exp']
+
+from .partialfrac import (t, thetas_alphas, thetas_alphas_to_expr,
+    thetas_alphas_to_expr_complex, allroots, MatrixNumPyPrinter, autoeye)
+
+__all__ += ['t', 'thetas_alphas', 'thetas_alphas_to_expr',
+    'thetas_alphas_to_expr_complex', 'allroots', 'MatrixNumPyPrinter', 'autoeye']
+
+from .util import diff_strs, relative_error
+
+__all__ += ['diff_strs', 'relative_error']
