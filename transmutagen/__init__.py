36f73f67a7f88c056d25fa1ee193979d5f9a7602
Aaron Meurer <asmeurer@gmail.com>
2017-01-30 17:53:46 -0500

    Fix imports in __init__.py

--- transmutagen/__init__.py
+++ transmutagen/__init__.py
@@ -1,8 +1,6 @@
-from .transmutagen import (general_rat_func, nsolve_intervals, nsolve_points,
-    plot_in_terminal, log_function_args, CRAM_exp)
+from .transmutagen import (general_rat_func, nsolve_intervals, nsolve_points, CRAM_exp)
 
-__all__ = ['general_rat_func', 'nsolve_intervals', 'nsolve_points',
-    'plot_in_terminal', 'log_function_args', 'CRAM_exp']
+__all__ = ['general_rat_func', 'nsolve_intervals', 'nsolve_points', 'CRAM_exp']
 
 from .partialfrac import (t, thetas_alphas, thetas_alphas_to_expr,
     thetas_alphas_to_expr_complex, allroots)
@@ -14,6 +12,8 @@ from .codegen import MatrixNumPyPrinter, autoeye, solve_with_autoeye
 
 __all__ += ['MatrixNumPyPrinter', 'autoeye', 'solve_with_autoeye']
 
-from .util import diff_strs, relative_error, mean_log10_relative_error
+from .util import (diff_strs, relative_error, mean_log10_relative_error,
+    log_function_args, plot_in_terminal)
 
-__all__ += ['diff_strs', 'relative_error', 'mean_log10_relative_error']
+__all__ += ['diff_strs', 'relative_error', 'mean_log10_relative_error',
+    'log_function_args', 'plot_in_terminal']
