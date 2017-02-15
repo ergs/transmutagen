caf29a40120dbc9cf0176abc358428ac544be7f4
Aaron Meurer <asmeurer@gmail.com>
2017-02-15 16:02:04 -0500

    Make build_an_run.sh work from other directories

--- docker/build_and_run.sh
+++ docker/build_and_run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 set -x
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
 docker build -f Dockerfile-base . -t origen-base
 docker build -f Dockerfile --no-cache . -t origen
 docker run origen
