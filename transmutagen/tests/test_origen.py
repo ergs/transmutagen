3f17f454172d15e9279e00ccc2acfb931bf685f1
Aaron Meurer <asmeurer@gmail.com>
2017-02-15 17:45:15 -0500

    Add a sanity test for the data

--- transmutagen/tests/test_origen.py
+++ transmutagen/tests/test_origen.py
@@ -16,14 +16,19 @@ def test_data():
     for datafile in os.listdir(DATA_DIR):
         data = load_data(os.path.join(DATA_DIR, datafile))
 
-        tape9, time, nuc, phi = datafile.split()[0]
+        tape9, time, nuc, phi = datafile.split()
 
         assert 'table_4' in data
         assert 'nuclide' in data['table_4']
 
-        nuclides = data['table_4']['nuclides']
+        nuclide = data['table_4']['nuclide']
 
         keys = ['activation_products', 'actinides', 'fission_products']
         # Sanity check
         for comb in combinations(keys, 2):
-            assert set.intersection(*comb) == set()
+            a, b = comb
+            for common in set.intersection(set(nuclide[a]), set(nuclide[b])):
+                array_a, array_b = nuclide[a][common], nuclide[b][common]
+                assert np.allclose(array_a, 0) \
+                    or np.allclose(array_b, 0)
+                    # or np.allclose(array_a, array_b)
