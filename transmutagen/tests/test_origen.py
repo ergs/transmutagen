707e47e58737ba490212deb539e8b6ac9180860a
Aaron Meurer <asmeurer@gmail.com>
2017-02-15 19:02:53 -0500

    Add WIP test_origen_against_CRAM

--- transmutagen/tests/test_origen.py
+++ transmutagen/tests/test_origen.py
@@ -2,33 +2,79 @@ import os
 from itertools import combinations
 
 import numpy as np
+from scipy.sparse import csr_matrix
+from sympy import symbols, lambdify
 
 from ..tape9utils import origen_to_name
+from ..partialfrac import (thetas_alphas, thetas_alphas_to_expr_complex, t,
+    multiply_vector)
+from ..codegen import MatrixNumPyPrinter, scipy_translations_autoeye
+from ..util import load_sparse_csr
+from .test_transmute import get_CRAM_from_cache
 
 DATA_DIR = os.path.abspath(os.path.join(__file__, os.path.pardir,
     os.path.pardir, os.path.pardir, 'docker', 'data'))
+
+NUCLIDE_KEYS = ['activation_products', 'actinides', 'fission_products']
 
 def load_data(datafile):
     with open(datafile) as f:
         return eval(f.read(), {'array': np.array})
 
+def origen_data_to_array(data, nucs):
+    nuclide = data['table_4']['nuclide']
+    new_data = np.zeros((len(nucs), 1))
+    nuc_to_idx = {v: i for i, v in enumerate(nucs)}
+    for key in NUCLIDE_KEYS:
+        for i in nuclide[key]:
+            new_data[nuc_to_idx[origen_to_name(i)]] += nuclide[key][i][1]
+
+    return new_data
+
 def test_data_sanity():
     for datafile in os.listdir(DATA_DIR):
         data = load_data(os.path.join(DATA_DIR, datafile))
-
-        tape9, time, nuc, phi = datafile.split()
+        tape9, time, nuc, phi = os.path.splitext(datafile)[0].split()
 
         assert 'table_4' in data
         assert 'nuclide' in data['table_4']
 
         nuclide = data['table_4']['nuclide']
 
-        keys = ['activation_products', 'actinides', 'fission_products']
         # Sanity check
-        for comb in combinations(keys, 2):
+        for comb in combinations(NUCLIDE_KEYS, 2):
             a, b = comb
             for common in set.intersection(set(nuclide[a]), set(nuclide[b])):
                 array_a, array_b = nuclide[a][common], nuclide[b][common]
                 assert np.allclose(array_a, 0) \
                     or np.allclose(array_b, 0)
                     # or np.allclose(array_a, array_b)
+
+def test_origen_against_CRAM():
+    for datafile in os.listdir(DATA_DIR):
+        origen_data = load_data(os.path.join(DATA_DIR, datafile))
+        tape9, time, nuc, phi = os.path.splitext(datafile)[0].split()
+
+
+        npzfilename = os.path.join('data', os.path.splitext(tape9)[0] + '_' + phi + '.npz')
+        nucs, mat = load_sparse_csr(npzfilename)
+
+        nuc_to_idx = {v: i for i, v in enumerate(nucs)}
+        b = csr_matrix(([1], [[nuc_to_idx[nuc]], [0]]), shape=[mat.shape[1],
+        1])
+
+        rat_func = get_CRAM_from_cache(14, 30)
+        thetas, alphas, alpha0 = thetas_alphas(rat_func, 30)
+        part_frac_complex = thetas_alphas_to_expr_complex(thetas, alphas, alpha0)
+        n0 = symbols("n0", commutative=False)
+
+        e_complex = lambdify((t, n0), multiply_vector(part_frac_complex, n0),
+            scipy_translations_autoeye, printer=MatrixNumPyPrinter({'use_autoeye': True
+                }))
+
+        CRAM_res = np.asarray(e_complex(-mat*float(time), b))
+
+        ORIGEN_res = origen_data_to_array(origen_data, nucs)
+
+        import pudb;pudb.set_trace()
+        np.testing.assert_allclose(CRAM_res, ORIGEN_res)
