4895cab7ebfea1767c4ce8ec8daa668f60f1396c
Aaron Meurer <asmeurer@gmail.com>
2017-02-17 19:14:40 -0500

    Functions to convert ORIGEN data in other ways

--- transmutagen/tests/test_origen.py
+++ transmutagen/tests/test_origen.py
@@ -51,6 +51,25 @@ def origen_data_to_array_weighted(data, nucs, n_fission_fragments=2.004):
 
     return new_data
 
+def origen_data_to_array_atom_fraction(data, nucs, n_fission_fragments=2):
+    # Table 4 is atom fraction
+    table_4_nuclide = data['table_4']['nuclide']
+    new_data = np.zeros((len(nucs), 1))
+    for key in NUCLIDE_KEYS:
+        new_data += origen_to_array(table_4_nuclide[key], nucs)
+
+    return new_data
+
+def origen_data_to_array_materials(data, nucs):
+    material = data['materials'][1]
+    new_data = np.zeros((len(nucs), 1))
+    nuc_to_idx = {v: i for i, v in enumerate(nucs)}
+
+    for nuc, atom_frac in material.to_atom_frac().items():
+        new_data[nuc_to_idx[pyne.nucname.name(nuc)]] = atom_frac
+
+    return new_data
+
 def test_data_sanity():
     for datafile in os.listdir(DATA_DIR):
         data = load_data(os.path.join(DATA_DIR, datafile))
@@ -94,7 +113,10 @@ def test_origen_against_CRAM():
         1])
 
         CRAM_res = np.asarray(e_complex(-mat.T*float(time), b))
+        CRAM_res_normalized = CRAM_res/np.sum(CRAM_res)
 
-        ORIGEN_res = origen_data_to_array(origen_data, nucs)
+        ORIGEN_res_weighted = origen_data_to_array_weighted(origen_data, nucs,)
+        ORIGEN_res_materials = origen_data_to_array_materials(origen_data, nucs)
+        ORIGEN_res_atom_fraction = origen_data_to_array_atom_fraction(origen_data, nucs)
 
         np.testing.assert_allclose(CRAM_res, ORIGEN_res)
