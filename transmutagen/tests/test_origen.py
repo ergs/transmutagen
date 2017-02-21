795f3d8a057d7587f9c70c758a45c0dbe95c4edd
Aaron Meurer <asmeurer@gmail.com>
2017-02-21 16:16:40 -0500

    Print some summary statistics in the ORIGEN test

--- transmutagen/tests/test_origen.py
+++ transmutagen/tests/test_origen.py
@@ -119,6 +119,17 @@ def test_origen_against_CRAM():
         ORIGEN_res_materials = origen_data_to_array_materials(origen_data, nucs)
         ORIGEN_res_atom_fraction = origen_data_to_array_atom_fraction(origen_data, nucs)
 
-        np.testing.assert_allclose(CRAM_res, ORIGEN_res_weighted, rtol=1e-3, atol=1e-6)
-        np.testing.assert_allclose(CRAM_res_normalized, ORIGEN_res_materials, rtol=1e-3, atol=1e-6)
-        np.testing.assert_allclose(CRAM_res_normalized, ORIGEN_res_atom_fraction, rtol=1e-3, atol=1e-6)
+        for C, O in [
+            (CRAM_res, ORIGEN_res_weighted),
+            (CRAM_res_normalized, ORIGEN_res_materials),
+            (CRAM_res_normalized, ORIGEN_res_atom_fraction),
+            ]:
+
+            try:
+                np.testing.assert_allclose(C, O, rtol=1e-3, atol=1e-5)
+            except AssertionError as e:
+                print(e)
+                for i in np.argsort(np.isclose(C, O, rtol=1e-3, atol=1e-6), axis=0)[:3]:
+                    print(nucs[i], C[i], O[i])
+
+        assert False
