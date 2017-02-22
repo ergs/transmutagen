8fac361f1523c1ae467894fcda717997eec0d93e
Aaron Meurer <asmeurer@gmail.com>
2017-02-22 17:56:15 -0500

    Cleanups to the test_origen

--- transmutagen/tests/test_origen.py
+++ transmutagen/tests/test_origen.py
@@ -100,10 +100,14 @@ def test_origen_against_CRAM():
         scipy_translations_autoeye, printer=MatrixNumPyPrinter({'use_autoeye': True
             }))
 
+    rtol=1e-3
+    atol=1e-6
+
     for datafile in os.listdir(DATA_DIR):
         origen_data = load_data(os.path.join(DATA_DIR, datafile))
         tape9, time, nuc, phi = os.path.splitext(datafile)[0].split()
-
+        print("Analyzing", tape9, "at time=", time, "nuc=", nuc, "phi=", phi)
+        print('-'*80)
 
         npzfilename = os.path.join('data', os.path.splitext(tape9)[0] + '_' + phi + '.npz')
         nucs, mat = load_sparse_csr(npzfilename)
@@ -117,24 +121,30 @@ def test_origen_against_CRAM():
 
         ORIGEN_res_weighted = origen_data_to_array_weighted(origen_data, nucs,)
         ORIGEN_res_materials = origen_data_to_array_materials(origen_data, nucs)
-        ORIGEN_res_atom_fraction = origen_data_to_array_atom_fraction(origen_data, nucs)
+        # ORIGEN_res_atom_fraction = origen_data_to_array_atom_fraction(origen_data, nucs)
 
-        for C, O, norm_type in [
-            (CRAM_res, ORIGEN_res_weighted, 'weighted'),
-            (CRAM_res_normalized, ORIGEN_res_materials, 'materials'),
-            (CRAM_res_normalized, ORIGEN_res_atom_fraction, 'atom fraction'),
+        for C, O, units in [
+            (CRAM_res, ORIGEN_res_weighted, 'atom fractions'),
+            (CRAM_res_normalized, ORIGEN_res_materials, 'mass frations'),
+            # (CRAM_res_normalized, ORIGEN_res_atom_fraction, 'atom fraction'),
             ]:
 
-            print("ORIGEN normalization:", norm_type)
+            print("Units:", units)
             try:
-                np.testing.assert_allclose(C, O, rtol=1e-3, atol=1e-5)
+                np.testing.assert_allclose(C, O, rtol=rtol, atol=atol)
             except AssertionError as e:
                 print(e)
-                print("Top mismatching elements (CRAM, ORIGEN)")
-                A = np.isclose(C, O, rtol=1e-3, atol=1e-6)
-                for i in np.argsort(A, axis=0)[np.sort(A, axis=0)==False]:
-                    print(nucs[i], C[i], O[i])
-                print()
+                print("Mismatching elements sorted by error (CRAM, ORIGEN, symmetric relative error)")
+                A = np.isclose(C, O, rtol=rtol, atol=atol)
+                rel_error = abs(C - O)/(C + O)
+                for i, in np.argsort(rel_error, axis=0)[::-1]:
+                    if A[i]:
+                        continue
+                    print(nucs[i], C[i], O[i], rel_error[i])
+            else:
+                print("Arrays match with rtol=", rtol, "atol=", atol)
+
+            print()
 
 if __name__ == '__main__':
     test_origen_against_CRAM()
