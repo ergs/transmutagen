6bc5e9a61b2f066d97c62a19202888fa1c6d3197
Aaron Meurer <asmeurer@gmail.com>
2017-03-13 18:10:39 -0400

    Basic runtimes plotting in the analysis script

--- transmutagen/analysis.py
+++ transmutagen/analysis.py
@@ -1,12 +1,31 @@
+from collections import defaultdict
 import tables
 
+import matplotlib.pyplot as plt
+
 def analyze(file):
+    times = {'origen': defaultdict(list), 'cram': defaultdict(list)}
     with tables.open_file(file, mode='r') as h5file:
-        for lib in h5file.root:
-            print(lib)
-            table = h5file.get_node(lib, '/origen')
-            for row in table:
-                print(row['execution time ORIGEN'])
+        for t in 'origen', 'cram':
+            for lib in h5file.root:
+                table = h5file.get_node(lib, t)
+                for row in table:
+                    times[t][row['time']].append(row['execution time ' + t.upper()])
+
+            xvals = range(len(times[t]))
+
+            x = []
+            y = []
+            for i in xvals:
+                itimes = times[t][sorted(times[t])[i]]
+                x += [i]*len(itimes)
+                y += itimes
+
+            plt.scatter(x, y)
+
+    plt.title('runtimes')
+    plt.semilogy()
+    plt.show()
 
 if __name__ == '__main__':
     import sys
