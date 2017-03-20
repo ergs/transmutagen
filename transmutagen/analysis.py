5215435a7b55f1ee63807529a81df0a8530c105f
Aaron Meurer <asmeurer@gmail.com>
2017-03-20 18:17:09 -0400

    Plot the time labels according to their values

--- transmutagen/analysis.py
+++ transmutagen/analysis.py
@@ -6,48 +6,48 @@ import argparse
 import tables
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import Formatter
 
 from .tests.test_transmute import run_transmute_test
 from .origen_all import TIME_STEPS
 
-TIME_LABELS = [
-    '1 second',
-    '1 day',
-    '1 month',
-    '1 year',
-    '10 years',
-    '1,000\nyears',
-    '1,000,000\nyears',
-]
+class TimeStepFormatter(Formatter):
+    def __call__(self, i, pos=None):
+        # TODO: Wrap the text
+        return TIME_STEPS.get(i, str(i))
 
 def analyze_origen(file):
+    fig, ax = plt.subplots()
+
     times = {'origen': defaultdict(list), 'cram': defaultdict(list)}
     with tables.open_file(file, mode='r') as h5file:
-        for t in 'origen', 'cram':
+        for run in 'origen', 'cram':
             for lib in h5file.root:
-                table = h5file.get_node(lib, t)
+                table = h5file.get_node(lib, run)
                 for row in table:
-                    times[t][row['time']].append(row['execution time ' + t.upper()])
+                    times[run][row['time']].append(row['execution time ' + run.upper()])
 
-            xvals = range(len(times[t]))
+            xvals = sorted(TIME_STEPS)
 
             x = []
             y = []
-            for i in xvals:
-                itimes = times[t][sorted(times[t])[i]]
-                x += [i]*len(itimes)
+            for i, t in enumerate(xvals):
+                itimes = times[run][sorted(times[run])[i]]
+                x += [t]*len(itimes)
                 y += itimes
 
-            plt.plot(x, y, 'o')
-
-        plt.xticks(xvals, TIME_LABELS, wrap=True)
+            ax.plot(x, y, 'o')
 
     # # Pad margins so that markers don't get clipped by the axes
     # plt.margins(0.2)
     # # Tweak spacing to prevent clipping of tick-labels
     # plt.subplots_adjust(bottom=0.15)
     plt.title('runtimes')
-    plt.semilogy()
+
+    ax.set_xscale('log')
+    ax.set_xticks(sorted(TIME_STEPS))
+    ax.get_xaxis().set_major_formatter(TimeStepFormatter())
+    ax.set_yscale('log')
 
     plt_show_in_terminal()
 
