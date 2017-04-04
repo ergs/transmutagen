bed9475845e36cb0825f909eff7c744e2ac07b5d
Aaron Meurer <asmeurer@gmail.com>
2017-04-04 17:31:50 -0400

    Add add7 and scalar_times_vector to __init__.py

--- py_solve/py_solve/__init__.py
+++ py_solve/py_solve/__init__.py
@@ -5,7 +5,8 @@ except ImportError:
     raise ImportError("Run 'python -m transmutagen.gensolve' and 'setup.py build_ext --inplace' to generate the module")
 
 from .py_solve import (N, NNZ, IJ, NUCS, NUCS_IDX, ROWS, COLS, ones,
-    flatten_sparse_matrix, csr_from_flat, asflat, solve, diag_add, dot)
+    flatten_sparse_matrix, csr_from_flat, asflat, solve, diag_add, dot, add7, scalar_times_vector)
 
 __all__ = ['N', 'NNZ', 'IJ', 'NUCS', 'NUCS_IDX', 'ROWS', 'COLS', 'ones',
-    'flatten_sparse_matrix', 'csr_from_flat', 'asflat', 'solve', 'diag_add', 'dot']
+    'flatten_sparse_matrix', 'csr_from_flat', 'asflat', 'solve', 'diag_add',
+    'dot', 'add7', 'scalar_times_vector']
