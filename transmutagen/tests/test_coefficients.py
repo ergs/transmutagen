4bd9f9f9a8563ff8ea3c5c23a7f905cb20e0e51d
Aaron Meurer <asmeurer@gmail.com>
2017-04-05 17:35:07 -0400

    Use the CRAM cache in test_coefficients

--- transmutagen/tests/test_coefficients.py
+++ transmutagen/tests/test_coefficients.py
@@ -8,14 +8,13 @@ slow = pytest.mark.skipif(
 TOTAL_DEGREES = 27
 
 from .crv_coeffs import coeffs as correct_coeffs
-# TODO: Should we use the CRAM cache here?
-from ..cram import CRAM_exp, CRAM_coeffs
+from ..cram import get_CRAM_from_cache, CRAM_coeffs
 
 @slow
 @pytest.mark.parametrize('degree', range(1, TOTAL_DEGREES+1))
 def test_coefficients(degree):
     generated_coeffs = {}
-    expr = CRAM_exp(degree, 30)
+    expr = get_CRAM_from_cache(degree, 30)
     generated_coeffs[degree] = CRAM_coeffs(expr, 20,
         decimal_rounding=True)
     assert generated_coeffs[degree] == correct_coeffs[degree]
