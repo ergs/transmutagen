66fbfc6a67e78027f78ca4084c233e0e08ee1c5d
Aaron Meurer <asmeurer@gmail.com>
2017-04-07 15:05:19 -0400

    Make the convergence value a mpmath number
    Otherwise, Python will round it to 0 for large precision values.

--- transmutagen/cram.py
+++ transmutagen/cram.py
@@ -193,7 +193,7 @@ def CRAM_exp(degree, prec=128, *, max_loops=10, c=None, maxsteps=None,
 
     maxsteps = int(maxsteps or 1.7*prec)
     tol = tol or mpmath.mpf(10)**-(prec - 8)
-    convergence_value = convergence_value or 10**-prec
+    convergence_value = convergence_value or mpmath.mpf(10)**-prec
 
     if nsolve_type == 'points':
         nsolve_func = nsolve_points
