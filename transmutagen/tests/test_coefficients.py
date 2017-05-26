75b02b3cafcc34dca143de9143f14c3c7c29c97c
Aaron Meurer <asmeurer@gmail.com>
2017-05-26 18:50:12 -0400

    Add test against Pusa coefficients (skipped for now, as they don't pass)

--- transmutagen/tests/test_coefficients.py
+++ transmutagen/tests/test_coefficients.py
@@ -1,4 +1,7 @@
+import decimal
+
 import pytest
+from sympy import re
 
 slow = pytest.mark.skipif(
     not pytest.config.getoption("--runslow"),
@@ -8,8 +11,9 @@ slow = pytest.mark.skipif(
 TOTAL_DEGREES = 30
 
 from .crv_coeffs import coeffs as correct_coeffs
+from .partfrac_coeffs import part_frac_coeffs
 from ..cram import get_CRAM_from_cache, CRAM_coeffs
-
+from ..partialfrac import thetas_alphas
 # @slow
 @pytest.mark.parametrize('degree', range(1, TOTAL_DEGREES+1))
 def test_coefficients(degree):
@@ -19,4 +23,24 @@ def test_coefficients(degree):
         decimal_rounding=True)
     # pytest won't show the full expr from the assert, so we print it too
     print(expr)
+    assert generated_coeffs[degree] == correct_coeffs[degree], expr
+
+@pytest.mark.xfail
+@pytest.mark.parametrize('degree', [14, 16])
+def test_partial_fraction_coefficients(degree):
+    generated_coeffs = {}
+    expr = get_CRAM_from_cache(degree, 200)
+    thetas, alphas, alpha0 = thetas_alphas(expr, 200)
+    format_str = '{:.19e}'
+    correct_coeffs = part_frac_coeffs[degree]
+    # Thetas in the paper are negative what we have
+    thetas = [-i for i in thetas]
+    for theta, real_theta, imag_theta in zip(sorted(thetas, key=re),
+        correct_coeffs['thetas']['real'],
+        correct_coeffs['thetas']['imaginary']):
+        real, imag = theta.as_real_imag()
+        assert format_str.format(decimal.Decimal(repr(real))) == real_theta
+        assert format_str.format(decimal.Decimal(repr(imag))) == imag_theta
+
+
     assert generated_coeffs[degree] == correct_coeffs[degree], expr
