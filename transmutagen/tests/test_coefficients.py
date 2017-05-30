8949f35c825994845ed72a9b271fbc6eceb5c808
Aaron Meurer <asmeurer@gmail.com>
2017-05-30 17:15:04 -0400

    Test alphas and alpha0
    Also, reverse the order of the parameterize decorators, as they are looped in
    reversed order, and change "correct_digits" to "paper_digits", as they may not
    actually be correct.

--- transmutagen/tests/test_coefficients.py
+++ transmutagen/tests/test_coefficients.py
@@ -32,26 +32,34 @@ def _thetas_alphas(expr, prec):
     return thetas_alphas(expr, prec)
 
 # @pytest.mark.xfail
-@pytest.mark.parametrize('degree', [14, 16])
-@pytest.mark.parametrize('idx', range(16//2))
 @pytest.mark.parametrize('real_imag', ['real', 'imag'])
-def test_partial_fraction_coefficients(degree, idx, real_imag):
+@pytest.mark.parametrize('idx', range(16//2))
+@pytest.mark.parametrize('typ', ['thetas', 'alphas', 'alpha0'])
+@pytest.mark.parametrize('degree', [14, 16])
+def test_partial_fraction_coefficients(degree, typ, idx, real_imag):
     if idx >= degree//2:
         # How do I do this in the decorators?
         return
     expr = get_CRAM_from_cache(degree, 200)
     thetas, alphas, alpha0 = _thetas_alphas(expr, 200)
     format_str = '{:+.19e}'
-    correct_coeffs = part_frac_coeffs[degree]
-    # Thetas in the paper are negative what we have, and are only counted once
-    # per conjugate.
-    thetas = [-i for i in thetas if im(-i) >= 0]
-    theta = sorted(thetas, key=im)[idx]
-    real_theta, imag_theta = sorted(zip(correct_coeffs['thetas']['real'],
-        correct_coeffs['thetas']['imaginary']), key=lambda i: float(i[1]))[idx]
+    paper_coeffs = part_frac_coeffs[degree]
+    # Thetas and alphas in the paper are negative what we have, and are only
+    # counted once per conjugate.
+    if typ == 'thetas':
+        vals = [-i for i in thetas if im(-i) >= 0]
+    elif typ == 'alphas':
+        vals = [-j for i, j in zip(thetas, alphas) if im(-i) >= 0]
+    elif typ == 'alpha0':
+        if idx != 0:
+            return
+        vals = [alpha0]
+    val = sorted(vals, key=im)[idx]
+    real_val_paper, imag_val_paper = sorted(zip(paper_coeffs[typ]['real'],
+        paper_coeffs[typ]['imaginary']), key=lambda i: float(i[1]))[idx]
 
-    real, imag = theta.as_real_imag()
+    real_val, imag_val = val.as_real_imag()
     if real_imag == 'real':
-        assert format_str.format(decimal.Decimal(repr(real))) == real_theta
+        assert format_str.format(decimal.Decimal(repr(real_val))) == real_val_paper
     else:
-        assert format_str.format(decimal.Decimal(repr(imag))) == imag_theta
+        assert format_str.format(decimal.Decimal(repr(imag_val))) == imag_val_paper
