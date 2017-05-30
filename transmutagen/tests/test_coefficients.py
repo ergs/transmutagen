5066d1274655c1c3ac4abd99a8dc79fa01c45158
Aaron Meurer <asmeurer@gmail.com>
2017-05-30 17:03:40 -0400

    Some improvements to test_partial_fraction_coefficients
    - Parameterize the test so that all digits are tested (only thetas so far)
    - Memoize the calls the thetas_alphas
    - Remove unrelated code from the function
    - Fix mismatching coefficients with positive sign
    - Comment out xfail (will be reverted)

--- transmutagen/tests/test_coefficients.py
+++ transmutagen/tests/test_coefficients.py
@@ -14,6 +14,8 @@ from .crv_coeffs import coeffs as correct_coeffs
 from .partfrac_coeffs import part_frac_coeffs
 from ..cram import get_CRAM_from_cache, CRAM_coeffs
 from ..partialfrac import thetas_alphas
+from ..util import memoize
+
 # @slow
 @pytest.mark.parametrize('degree', range(1, TOTAL_DEGREES+1))
 def test_coefficients(degree):
@@ -25,24 +27,31 @@ def test_coefficients(degree):
     print(expr)
     assert generated_coeffs[degree] == correct_coeffs[degree], expr
 
-@pytest.mark.xfail
+@memoize
+def _thetas_alphas(expr, prec):
+    return thetas_alphas(expr, prec)
+
+# @pytest.mark.xfail
 @pytest.mark.parametrize('degree', [14, 16])
-def test_partial_fraction_coefficients(degree):
-    generated_coeffs = {}
+@pytest.mark.parametrize('idx', range(16//2))
+@pytest.mark.parametrize('real_imag', ['real', 'imag'])
+def test_partial_fraction_coefficients(degree, idx, real_imag):
+    if idx >= degree//2:
+        # How do I do this in the decorators?
+        return
     expr = get_CRAM_from_cache(degree, 200)
-    thetas, alphas, alpha0 = thetas_alphas(expr, 200)
-    format_str = '{:.19e}'
+    thetas, alphas, alpha0 = _thetas_alphas(expr, 200)
+    format_str = '{:+.19e}'
     correct_coeffs = part_frac_coeffs[degree]
     # Thetas in the paper are negative what we have, and are only counted once
     # per conjugate.
     thetas = [-i for i in thetas if im(-i) >= 0]
-    for theta, (real_theta, imag_theta) in zip(sorted(thetas, key=im),
-        sorted(zip(correct_coeffs['thetas']['real'],
-            correct_coeffs['thetas']['imaginary']), key=lambda i: float(i[1]))):
+    theta = sorted(thetas, key=im)[idx]
+    real_theta, imag_theta = sorted(zip(correct_coeffs['thetas']['real'],
+        correct_coeffs['thetas']['imaginary']), key=lambda i: float(i[1]))[idx]
 
-        real, imag = theta.as_real_imag()
+    real, imag = theta.as_real_imag()
+    if real_imag == 'real':
         assert format_str.format(decimal.Decimal(repr(real))) == real_theta
+    else:
         assert format_str.format(decimal.Decimal(repr(imag))) == imag_theta
-
-
-    assert generated_coeffs[degree] == correct_coeffs[degree], expr
