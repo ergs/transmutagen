943a685ffaee77714c39c1fd16f4e5ee68f5196d
Aaron Meurer <asmeurer@gmail.com>
2017-07-17 13:45:18 -0400

    Add build_and_run.sh for cram

---
+++ docker/cram/build_and_run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+set -x
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+docker build -f Dockerfile . -t cram
+docker run -v "$parent_path/../../logs":/logs -v "$parent_path/../../CRAM_cache":~/.transmutagen/CRAM_cache cram --save-cache "$@"
