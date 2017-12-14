c3da8fa380476e5caaff2fada42c1ea9bf58566a
Aaron Meurer <asmeurer@gmail.com>
2017-12-13 21:25:21 -0500

    Fix CRAM docker build_and_run.sh

--- docker/cram/build_and_run.sh
+++ docker/cram/build_and_run.sh
@@ -6,4 +6,4 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
 docker build -f Dockerfile . -t ergs/cram
-docker run -v "$parent_path/../../logs":/root/transmutagen/logs -v "$parent_path/../../plots":/root/transmutagen/plots -v "$parent_path/../../CRAM_cache":/root/.transmutagen/CRAM_cache cram --save-cache "$@"
+docker run -v "$parent_path/../../logs":/root/transmutagen/logs -v "$parent_path/../../plots":/root/transmutagen/plots -v "$parent_path/../../CRAM_cache":/root/.transmutagen/CRAM_cache ergs/cram --save-cache "$@"
