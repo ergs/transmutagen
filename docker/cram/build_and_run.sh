b48594a9a671b8137cc6d854a3adea3344b5699a
Aaron Meurer <asmeurer@gmail.com>
2017-12-13 20:53:29 -0500

    Revert "Don't force a rebuild in the CRAM docker"
    This reverts commit 7555e33bf9557024a0e0ba936412b60b8cb7770c.
    
    I guess -f means file, not force.

--- docker/cram/build_and_run.sh
+++ docker/cram/build_and_run.sh
@@ -5,5 +5,5 @@ set -x
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-docker build Dockerfile . -t ergs/cram
+docker build -f Dockerfile . -t ergs/cram
 docker run -v "$parent_path/../../logs":/root/transmutagen/logs -v "$parent_path/../../plots":/root/transmutagen/plots -v "$parent_path/../../CRAM_cache":/root/.transmutagen/CRAM_cache cram --save-cache "$@"
