1e9fc09d165077f53e3e868a5ccd0e2967c9bffc
Aaron Meurer <asmeurer@gmail.com>
2018-03-20 15:41:30 -0400

    Move py_solve into transmutagen
    So far I fixed the tests file, but I haven't yet fixed setup.py or the imports.

--- transmutagen/py_solve/tests/test_solve.py
+++ transmutagen/py_solve/tests/test_solve.py
@@ -5,7 +5,7 @@ import numpy as np
 import scipy.sparse as sp
 import scipy.sparse.linalg as spla
 
-import py_solve as solver
+from .. import N, solve, ones, flatten_sparse_matrix, diag_add, dot
 
 import pytest
 
@@ -17,63 +17,63 @@ DTYPES = ['f8', np.complex128]
 
 @pytest.mark.parametrize('dtype', DTYPES)
 def test_solve_identity_ones(dtype):
-    b = np.ones(solver.N, dtype=dtype)
-    mat = sp.eye(solver.N, format='csr', dtype=dtype)
-    obs = solver.solve(mat, b)
+    b = np.ones(N, dtype=dtype)
+    mat = sp.eye(N, format='csr', dtype=dtype)
+    obs = solve(mat, b)
     exp = spla.spsolve(mat, b)
     assert np.allclose(exp, obs)
 
 
 @pytest.mark.parametrize('dtype', DTYPES)
 def test_solve_identity_range(dtype):
-    b = np.arange(solver.N, dtype=dtype)
-    mat = sp.eye(solver.N, format='csr', dtype=dtype)
-    obs = solver.solve(mat, b)
+    b = np.arange(N, dtype=dtype)
+    mat = sp.eye(N, format='csr', dtype=dtype)
+    obs = solve(mat, b)
     exp = spla.spsolve(mat, b)
     assert np.allclose(exp, obs)
 
 
 @pytest.mark.parametrize('dtype', DTYPES)
 def test_solve_ones_ones(dtype):
-    b = np.ones(solver.N, dtype=dtype)
-    mat = solver.ones(dtype=dtype) + 9*sp.eye(solver.N, format='csr', dtype=dtype)
-    obs = solver.solve(mat, b)
+    b = np.ones(N, dtype=dtype)
+    mat = ones(dtype=dtype) + 9*sp.eye(N, format='csr', dtype=dtype)
+    obs = solve(mat, b)
     exp = spla.spsolve(mat, b)
     assert np.allclose(exp, obs)
 
 
 @pytest.mark.parametrize('dtype', DTYPES)
 def test_solve_ones_range(dtype):
-    b = np.arange(solver.N, dtype=dtype)
-    mat = solver.ones(dtype=dtype) + 9*sp.eye(solver.N, format='csr', dtype=dtype)
-    obs = solver.solve(mat, b)
+    b = np.arange(N, dtype=dtype)
+    mat = ones(dtype=dtype) + 9*sp.eye(N, format='csr', dtype=dtype)
+    obs = solve(mat, b)
     exp = spla.spsolve(mat, b)
     assert np.allclose(exp, obs)
 
 
 @pytest.mark.parametrize('dtype', DTYPES)
 def test_solve_range_range(dtype):
-    b = np.arange(solver.N, dtype=dtype)
-    mat = solver.ones(dtype=dtype) + sp.diags([b], offsets=[0], shape=(solver.N, solver.N),
+    b = np.arange(N, dtype=dtype)
+    mat = ones(dtype=dtype) + sp.diags([b], offsets=[0], shape=(N, N),
                                               format='csr', dtype=dtype)
-    obs = solver.solve(mat, b)
+    obs = solve(mat, b)
     exp = spla.spsolve(mat, b)
     assert np.allclose(exp, obs)
 
 
 @pytest.mark.parametrize('dtype', DTYPES)
 def test_diag_add(dtype):
-    mat = solver.ones(dtype=dtype)
-    res = mat + 9*sp.eye(solver.N, format='csr', dtype=dtype)
-    exp = solver.flatten_sparse_matrix(res)
-    obs = solver.diag_add(mat, 9.0)
+    mat = ones(dtype=dtype)
+    res = mat + 9*sp.eye(N, format='csr', dtype=dtype)
+    exp = flatten_sparse_matrix(res)
+    obs = diag_add(mat, 9.0)
     assert np.allclose(exp, obs)
 
 
 @pytest.mark.parametrize('dtype', DTYPES)
 def test_dot(dtype):
-    x = np.arange(solver.N, dtype=dtype)
-    mat = solver.ones(dtype=dtype) + 9*sp.eye(solver.N, format='csr', dtype=dtype)
+    x = np.arange(N, dtype=dtype)
+    mat = ones(dtype=dtype) + 9*sp.eye(N, format='csr', dtype=dtype)
     exp = mat.dot(x)
-    obs = solver.dot(mat, x)
+    obs = dot(mat, x)
     assert np.allclose(exp, obs)
